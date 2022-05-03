0d9fa22a28b10fac9586efa5b066e499e7798e9c
Jora <jora_a@outlook.com>
2022-05-03 19:58:24 +0430

    Add authenticate middleware

--- src/app/middleware/authenticate.auth.ts
+++ src/app/middleware/authenticate.auth.ts
@@ -1,8 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UsersService } from "../../modules/users/user.service";
+import { catchErrors } from "../errors/asyncCatch";
 
-const authenticate = (req: Request, res: Response, next: NextFunction) => {
-    console.log('this is auth middleware');
-    next();
-};
+const authenticate = catchErrors(async (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.params;
+    const { token } = req.body;
+
+    const data: any = UsersService.verifyToken(token);
+
+    const user = await UsersService.getUserByToken(token, data.email);
+
+    if (data.email === email && user) {
+        return next();
+    }
+
+    throw new Error('Not Authenticated');
+});
 
 export default authenticate;
